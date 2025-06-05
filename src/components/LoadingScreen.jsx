3ae997ed90951314f58b809b70904e3ddd4364b9
venkadesh-developer <venkadesh1102@gmail.com>
2025-06-05 14:46:26 +0530

    My portfolio

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -24,7 +24,7 @@ export const LoadingScreen = ({ onComplete }) => {
   }, [onComplete]);
 
   return (
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center p-15">
       <div className="mb-4 text-4xl font-mono font-bold">
         {text} <span className="animate-blink ml-1"> | </span>
       </div>
